17604d408d5e3db4d6e4e016d088193781396a03
Alex <alexander.bueso@hilliardschools.org>
2018-04-20 12:11:44 -0400

    new
    new

--- Menu.py
+++ Menu.py
@@ -2,9 +2,10 @@
 Menu by Alex B
 4/5/18
 '''
+answer = ""
 totalPrice = 0
 menu = {"sword": 30, "shield": 45, "spear": 20, "duel-daggers": 15, "guantlets": 40}
-print("Welcome to the Weapon shop. Here we sell weapons so you can murder. Type quit at any point to leave")
+print("Welcome to the Weapon shop. Here we sell weapons so you can murder. Type quit at any point to finish ordering")
 while True:
     print(''' ''')
     print('''Sword: $30
@@ -14,33 +15,27 @@ Duel-Daggers: $15
 Gauntlets: $40
     ''')
     answer = input("What would you like to order? ").lower()
-    if answer == ("quit"):
-        print("See ya!")
-        break
-    if answer not in menu:
-        print("We don't sell that")
-        continue
-    if answer in menu:
-        break
-while True:
-    HowMany = input("How many would you like? ")
-    try:
-        HowMany = int(HowMany)
-        break
-    except:
-        print("Sorry, can't do that")
-        
+    while answer != "quit":
+        if answer == "quit":
+            print("See ya!")
+            break
+        if answer not in menu:
+            print("We don't sell that")
+            continue
+        if answer in menu:
+            break
+    while True:
+        HowMany = input("How many would you like? ")
+        try:
+            HowMany = int(HowMany)
+            break
+        except:
+            print("Sorry, can't do that")
+            continue
 
-totalPrice = menu[answer] * HowMany
+        totalPrice += menu[answer] * HowMany
 print(totalPrice, "Coins")
 while True:
-    cont = input("Have you finished your order? ").lower()
-    if cont != "yes" and cont != "no":
-        print("Yes or No?")
-        continue
-    if cont == "no":
-        break
-    if cont == "yes":
         share = input("Are you splitting the price ").lower()
         if share != "yes" and share!= "no":
              print("Yes or No?")
@@ -49,7 +44,7 @@ while True:
             split = input("With how many? ")
             try:
                 split = float(split)
-            
+
             except:
                 print("Gimme a number")
             totalPrice = totalPrice / split
